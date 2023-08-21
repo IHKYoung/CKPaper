7cea3cad423a6350b08b028b5b0e015c40cad169
YOUNG <iyangkun@ruc.edu.cn>
2023-08-21 22:26:02 +0800

    :tada: Happy New CKPaper

---
+++ assets/js/license.js
@@ -0,0 +1,6 @@
+/*
+  CKPaper v7
+  License: MIT https://github.com/IHKYoung/CKPaper.git/blob/master/LICENSE
+  Copyright (c) 2020 nanxiaobei and adityatelange
+  Copyright (c) 2021-2023 adityatelange
+*/
