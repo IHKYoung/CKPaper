e7deb79f2b7bfb3ec4d1d56ce0d293a7f61f3888
YOUNG <iyangkun@ruc.edu.cn>
2024-03-26 16:38:40 +0800

    Reset

--- assets/css/core/license.css
+++ assets/css/core/license.css
@@ -1,6 +1,6 @@
 /*
-  CKPaper v7
-  License: MIT https://github.com/IHKYoung/CKPaper.git/blob/master/LICENSE
+  PaperMod v7
+  License: MIT https://github.com/adityatelange/hugo-PaperMod/blob/master/LICENSE
   Copyright (c) 2020 nanxiaobei and adityatelange
-  Copyright (c) 2021-2023 adityatelange
+  Copyright (c) 2021-2024 adityatelange
 */
